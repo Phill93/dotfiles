8d4b8ff7a2ef75a1d0d048c8a6f007a060fdb427
Daniel Bacher <mail@phill93.de>
2020-01-03 14:31:03 +0100

    fix setup

--- setup.sh
+++ setup.sh
@@ -5,10 +5,10 @@ BASEDIR=$(dirname "$0")
 sudo pacman -S zsh vim curl
 pikaur -S oh-my-zsh-git
 
-ln -s $BASEDIR/zshrc ~/.zshrc
-ln -s $BASEDIR/aliases ~/.aliases
-ln -s $BASEDIR/functions ~/.functions
-ln -s $BASEDIR/gitignore ~/.gitignore
-mkidr -p ~/.gnupg
-ln -s $BASEDIR/gpg.conf ~/.gnupg/gpg.conf
+ln -fs $BASEDIR/zshrc ~/.zshrc
+ln -fs $BASEDIR/aliases ~/.aliases
+ln -fs $BASEDIR/functions ~/.functions
+ln -fs $BASEDIR/gitignore ~/.gitignore
+mkdir -p ~/.gnupg
+ln -fs $BASEDIR/gpg.conf ~/.gnupg/gpg.conf
 curl -sSL https://phill93.de/gpg.pub.asc | gpg --import -
